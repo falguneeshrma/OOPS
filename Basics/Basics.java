4fed0aad187db054cd8db1746bf2293c4f3a4d56
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-04-13 19:09:56 +0530

    Basic concept revison

---
+++ Basics/Basics.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Basics {
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+
+        // short num = 100;
+        // byte num = 100;
+        // int num = 100;
+        // long num = 1000;
+        // float num = 1.1f;
+        // double num = 11.11;
+        char letter = 'a';
+        System.out.println(letter);
+    }
+}
+
