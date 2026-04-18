ca946d82e802ed79614b17db1d8addf4be97ca79
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-04-18 14:07:34 +0530

    Real life examples

--- Introduction/AttributesAndMethods.java
+++ Introduction/AttributesAndMethods.java
@@ -19,6 +19,42 @@ class BankAccount{
     public String getName(){
         return name;
     }
+    public double getBalance(){
+        return balance;
+    }
+    public double addMoney(double amount){
+        try{
+            this.balance += amount;
+        }
+        catch(Exception e){
+            System.out.println("Bank Server Down. Try again later.");
+        }  
+        
+        return amount;
+    }
+
+    public double withdrawMoney(double amount){
+        try{
+            if(balance >= amount){
+                balance -= amount;
+            }
+            else{
+                System.out.println("Your account balance is insufficient.");
+            }
+        }
+        catch(Exception e){
+            System.out.println("Bank Server Down. Try again later.");
+        }  
+        
+        return amount;
+    }
+
+
+    //default constructor
+    BankAccount(){
+        name = "anonymous";
+        balance = 0;
+    }
 }
 
 public class AttributesAndMethods {
@@ -28,5 +64,22 @@ public class AttributesAndMethods {
 
         BankAccount cust1 = new BankAccount("Rahul", 50000); //constructor called
         System.out.println("Name: " + cust1.getName());
+        System.out.println("Current Balance: " + cust1.getBalance());
+        System.out.println("Credit account by sum of: " + cust1.addMoney(1000));
+        System.out.println("Current Balance: " + cust1.getBalance());
+
+        BankAccount cust0 = new BankAccount();  //constructor called
+        System.out.println("Name: " + cust0.getName());
+        System.out.println("Current Balance: " + cust0.getBalance());
+        System.out.println("Credit account by sum of: " + cust0.addMoney(1000));
+        System.out.println("Current Balance: " + cust0.getBalance());
+        System.out.println("Debit account by sum of: " + cust0.withdrawMoney(1500));
+        System.out.println("Current Balance: " + cust0.getBalance());
+
+        BankAccount cust2 = new BankAccount("Anand", 60000); //constructor called
+        System.out.println("Name: " + cust2.getName());
+        System.out.println("Current " + cust2.getBalance());
+        System.out.println("Credit account by sum of: " + cust2.addMoney(5000));
+        System.out.println("Current Balance: " + cust2.getBalance());
     }
 }
