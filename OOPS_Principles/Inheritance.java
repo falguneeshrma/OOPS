5f4affef6eaa24f30ef2d70df578889a225ab07c
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-04-24 22:15:46 +0530

    Real life example

--- OOPS_Principles/Inheritance.java
+++ OOPS_Principles/Inheritance.java
@@ -145,6 +145,53 @@ class Tshirts extends Product{
     
 }
 
+// Real life example - employee hierarchy
+class Employee {
+    protected String name;
+    protected int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name : " + name);
+        System.out.println("Id : " + id);
+    }
+}
+
+class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(String name, int id, int teamSize) {
+        super(name, id);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails(); 
+        System.out.println("Team Size : " + teamSize);
+    }
+}
+class Engineer extends Employee {
+    private String specialization;
+
+    public Engineer(String name, int id, String specialization) {
+        super(name, id);
+        this.specialization = specialization;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Specialization : " + specialization);
+    }
+}
+
+
+
 class Inheritance{
     public static void main(String[] args){
 
@@ -179,14 +226,28 @@ class Inheritance{
 
         // System.out.println("--------------------- ");
 
-        Electronics electronics = new Electronics(2, "HP", "Laptop", 60000);
-        electronics.displayDetails();
+        // Electronics electronics = new Electronics(2, "HP", "Laptop", 60000);
+        // electronics.displayDetails();
 
-        System.out.println("--------------------- ");
+        // System.out.println("--------------------- ");
 
-        Tshirts tshirts = new Tshirts("blue", "M", "Polo", 1000);
-        tshirts.displayDetails();
+        // Tshirts tshirts = new Tshirts("blue", "M", "Polo", 1000);
+        // tshirts.displayDetails();
         
+
+        String M_name = "Jax";
+        int M_id = 101;
+        int M_teamSize = 8;
+        String E_name = "William";
+        int E_id = 202;
+        String E_specialization = "Backend Developer";
+        Manager manager = new Manager(M_name, M_id, M_teamSize);
+        System.out.println("Manager Details");
+        manager.displayDetails();
+        System.out.println();
+        Engineer engineer = new Engineer(E_name, E_id, E_specialization);
+        System.out.println("Engineer Details");
+        engineer.displayDetails();
     }
     
     
