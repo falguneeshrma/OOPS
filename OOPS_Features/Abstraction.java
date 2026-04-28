7c9ad3cb42a3ace54307817b5994b92ffd1e0da3
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-04-28 18:30:00 +0530

    Abstraction using classes and interfaces, default and static methods in interfaces, inheritance in abstract classes, constructors of abstract classes

--- OOPS_Features/Abstraction.java
+++ OOPS_Features/Abstraction.java
@@ -3,13 +3,47 @@ import java.util.*;
 
 //Abstraction
 abstract class Car{
+
+    //constructor
+    Car(){
+        System.out.println("This is a car constructor.");
+    }
+
     abstract public void start();
+
     public void honk(){
         System.out.println("Peeep...Peeep!");
     }
+
+    public void sunroof(){
+        System.out.println("Car has a sunroof!");
+    }
 }
 
-class ManualCar extends Car{
+//hierarchical inheritance - Car, Dealer, ManualCar, AutomaticCar
+
+abstract class Dealer extends Car{
+    int numberOfCars;
+
+    //constructor
+    Dealer(){
+        System.out.println("This is a dealer constructor.");
+    }
+    Dealer(int numberOfCars){
+        System.out.println("This is a parameterised constructor.");
+        this.numberOfCars = numberOfCars;
+    }
+}
+
+class ManualCar extends Dealer{
+
+    //constructor
+    ManualCar(){
+        super(12);
+        System.out.println("This is a manual car constructor.");
+    }
+
+    @Override
     public void start(){
         System.out.println("Manual car is starting!");
     }
@@ -18,9 +52,47 @@ class ManualCar extends Car{
     }   
 }
 
-class AutomaticCar extends Car{
+class AutomaticCar extends Dealer{
+//constructor
+    AutomaticCar(){
+        System.out.println("This is a automatic car constructor.");
+    }
+
+    @Override
     public void start(){
         System.out.println("Automatic car is starting!");
+    }
+}
+
+
+//interface 
+
+interface CarInterface{
+    public void start();
+
+    //after Java 8 - default and static
+
+    //default
+    default void honk(){
+        System.out.println("Peeep...Peeep!");
+    }
+
+    //static
+    static int numberOfWheels(){
+        System.out.println("Wheels: 4");
+        return 4; // no need to create instance
+    }
+}
+
+class ElectricCar implements CarInterface{
+    //constructor
+    ElectricCar(){
+        System.out.println("This is a electric car constructor.");
+    }
+
+    @Override
+    public void start(){
+        System.out.println("Electric car is starting!");
     }
 }
 
@@ -30,6 +102,7 @@ class Abstraction{
         Car car = new ManualCar();
         car.start();
         car.honk();
+        car.sunroof();
 
         System.out.println("---------------");
         
@@ -37,11 +110,22 @@ class Abstraction{
         manualCar.start();
         manualCar.honk();
         manualCar.playMusic();
+        manualCar.sunroof();
 
         System.out.println("---------------");
         
         AutomaticCar autoCar = new AutomaticCar();
         autoCar.start();
         autoCar.honk();
+        autoCar.sunroof();
+
+        System.out.println("---------------");
+        
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.start();
+
+        System.out.println("---------------");
+        
+        CarInterface.numberOfWheels();
     }
 }
