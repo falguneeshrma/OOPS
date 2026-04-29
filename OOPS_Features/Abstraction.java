0cbf94e34be749b27b24ee1ad14fc2c656cfadc6
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-04-29 15:19:38 +0530

    Real World Example

--- OOPS_Features/Abstraction.java
+++ OOPS_Features/Abstraction.java
@@ -2,130 +2,174 @@ package OOPS_Features;
 import java.util.*;
 
 //Abstraction
-abstract class Car{
+// abstract class Car{
 
-    //constructor
-    Car(){
-        System.out.println("This is a car constructor.");
-    }
+//     //constructor
+//     Car(){
+//         System.out.println("This is a car constructor.");
+//     }
 
-    abstract public void start();
+//     abstract public void start();
 
-    public void honk(){
-        System.out.println("Peeep...Peeep!");
-    }
+//     public void honk(){
+//         System.out.println("Peeep...Peeep!");
+//     }
 
-    public void sunroof(){
-        System.out.println("Car has a sunroof!");
-    }
-}
+//     public void sunroof(){
+//         System.out.println("Car has a sunroof!");
+//     }
+// }
 
 //hierarchical inheritance - Car, Dealer, ManualCar, AutomaticCar
 
-abstract class Dealer extends Car{
-    int numberOfCars;
+// abstract class Dealer extends Car{
+//     int numberOfCars;
 
-    //constructor
-    Dealer(){
-        System.out.println("This is a dealer constructor.");
-    }
-    Dealer(int numberOfCars){
-        System.out.println("This is a parameterised constructor.");
-        this.numberOfCars = numberOfCars;
-    }
-}
+//     //constructor
+//     Dealer(){
+//         System.out.println("This is a dealer constructor.");
+//     }
+//     Dealer(int numberOfCars){
+//         System.out.println("This is a parameterised constructor.");
+//         this.numberOfCars = numberOfCars;
+//     }
+// }
 
-class ManualCar extends Dealer{
+// class ManualCar extends Dealer{
 
-    //constructor
-    ManualCar(){
-        super(12);
-        System.out.println("This is a manual car constructor.");
-    }
+//     //constructor
+//     ManualCar(){
+//         super(12);
+//         System.out.println("This is a manual car constructor.");
+//     }
 
-    @Override
-    public void start(){
-        System.out.println("Manual car is starting!");
-    }
-    public void playMusic(){
-        System.out.println("Manual car is now playing music!");
-    }   
-}
+//     @Override
+//     public void start(){
+//         System.out.println("Manual car is starting!");
+//     }
+//     public void playMusic(){
+//         System.out.println("Manual car is now playing music!");
+//     }   
+// }
 
-class AutomaticCar extends Dealer{
-//constructor
-    AutomaticCar(){
-        System.out.println("This is a automatic car constructor.");
-    }
+// class AutomaticCar extends Dealer{
+// //constructor
+//     AutomaticCar(){
+//         System.out.println("This is a automatic car constructor.");
+//     }
 
-    @Override
-    public void start(){
-        System.out.println("Automatic car is starting!");
-    }
-}
+//     @Override
+//     public void start(){
+//         System.out.println("Automatic car is starting!");
+//     }
+// }
 
 
 //interface 
 
-interface CarInterface{
-    public void start();
+// interface CarInterface{
+//     public void start();
 
-    //after Java 8 - default and static
+//     //after Java 8 - default and static
 
-    //default
-    default void honk(){
-        System.out.println("Peeep...Peeep!");
+//     //default
+//     default void honk(){
+//         System.out.println("Peeep...Peeep!");
+//     }
+
+//     //static
+//     static int numberOfWheels(){
+//         System.out.println("Wheels: 4");
+//         return 4; // no need to create instance
+//     }
+// }
+
+// class ElectricCar implements CarInterface{
+//     //constructor
+//     ElectricCar(){
+//         System.out.println("This is a electric car constructor.");
+//     }
+
+//     @Override
+//     public void start(){
+//         System.out.println("Electric car is starting!");
+//     }
+// }
+
+
+abstract class Animal{
+    protected String name;
+
+    Animal(String name){
+        this.name = name;
     }
 
-    //static
-    static int numberOfWheels(){
-        System.out.println("Wheels: 4");
-        return 4; // no need to create instance
-    }
+    abstract public void makeSound();
 }
 
-class ElectricCar implements CarInterface{
-    //constructor
-    ElectricCar(){
-        System.out.println("This is a electric car constructor.");
+class Dog extends Animal{
+    Dog(String name){
+        super(name);
     }
 
     @Override
-    public void start(){
-        System.out.println("Electric car is starting!");
+    public void makeSound(){
+        System.out.println("The dog " + name + " says : Woof!");
+    }
+}
+
+class Cat extends Animal{
+    Cat(String name){
+        super(name);
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("The cat " + name + " says : Meow!");
     }
 }
 
 class Abstraction{
     public static void main(String[] args){
 
-        Car car = new ManualCar();
-        car.start();
-        car.honk();
-        car.sunroof();
+        // Car car = new ManualCar();
+        // car.start();
+        // car.honk();
+        // car.sunroof();
 
-        System.out.println("---------------");
+        // System.out.println("---------------");
         
-        ManualCar manualCar = new ManualCar();
-        manualCar.start();
-        manualCar.honk();
-        manualCar.playMusic();
-        manualCar.sunroof();
+        // ManualCar manualCar = new ManualCar();
+        // manualCar.start();
+        // manualCar.honk();
+        // manualCar.playMusic();
+        // manualCar.sunroof();
 
-        System.out.println("---------------");
+        // System.out.println("---------------");
         
-        AutomaticCar autoCar = new AutomaticCar();
-        autoCar.start();
-        autoCar.honk();
-        autoCar.sunroof();
+        // AutomaticCar autoCar = new AutomaticCar();
+        // autoCar.start();
+        // autoCar.honk();
+        // autoCar.sunroof();
 
-        System.out.println("---------------");
+        // System.out.println("---------------");
         
-        ElectricCar electricCar = new ElectricCar();
-        electricCar.start();
+        // ElectricCar electricCar = new ElectricCar();
+        // electricCar.start();
 
-        System.out.println("---------------");
+        // System.out.println("---------------");
         
-        CarInterface.numberOfWheels();
+        // CarInterface.numberOfWheels();
+
+        String cName = "Whiskers";
+        String dName = "Buddy";
+
+        // String cName = "Sweetie";
+        // String dName = "Rocky";
+
+        Animal dog = new Dog(dName);
+        dog.makeSound();
+        Animal cat = new Cat(cName);
+        cat.makeSound();
     }
 }
