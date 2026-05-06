6b0e5479dfde929af46c3881ac37efaffa79c1de
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-05-06 22:58:17 +0530

    Real World Example

--- OOPS_Features/InnerClasses.java
+++ OOPS_Features/InnerClasses.java
@@ -76,6 +76,57 @@ import java.util.*;
 
 // Real World Example
 
+class Robot{
+    private String name;
+
+    public Robot(String name){
+        this.name = name;
+    }
+
+    public void performAction(){
+        System.out.println(name + " is performing an action.");
+    }
+
+    class Arm{
+        public void pickItem(){
+            System.out.println(name + " arm picking an item.");
+        }
+    }
+
+    static class Processor{
+        public void process(){
+            System.out.println("Processor analyzing the data.");
+        }
+    }
+
+    public void manageSensors(){
+        class Sensor{
+            public void sense(){
+                System.out.println(name + " sensor detecting obstacles.");
+            }
+        }
+
+        Sensor sensor = new Sensor();
+        sensor.sense();
+
+    }
+
+    interface Task{
+        public void execute();
+    }
+
+    public void executeTask() {
+        Task task = new Task() {
+            @Override
+            public void execute() {
+                System.out.println(name + " executing a custom task.");
+            }
+        };
+        task.execute();
+    }
+
+}
+
 
 
 
@@ -134,6 +185,20 @@ class InnerClasses{
         list.add(18);
         list.add(7);
         System.out.println(list);
+
+        System.out.println();
+        System.out.println("------------Real World Example------------");
+
+        String name = "Robot-1";
+        Robot robot = new Robot(name);
+        robot.performAction();
+        Robot.Arm arm = robot.new Arm();
+        arm.pickItem();
+        Robot.Processor processor = new Robot.Processor();
+        processor.process();
+        robot.manageSensors();
+        robot.executeTask();
+
 
     }
 }
