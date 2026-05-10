4561491afc6149f73458edff8a59a7e9c646d887
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-05-10 23:50:05 +0530

    Association Relationship: many-to-many

--- RelationshipsAndObjectBehavior/Association.java
+++ RelationshipsAndObjectBehavior/Association.java
@@ -36,31 +36,77 @@ import java.util.*;
 
 //one to many relationship
 
-class Student{
-    private String name;  
-    private String id; 
+// class Student{
+//     private String name;  
+//     private String id; 
 
-    //constructor
-    Student(String name,String id){
+//     //constructor
+//     Student(String name,String id){
+//         this.name = name;
+//         this.id = id;
+//     }
+
+//     public String getName(){
+//         return name;
+//     }
+
+//     public String getId(){
+//         return id;
+//     }
+// }
+
+// class College{
+//     private String name;   
+//     private List<Student> students;
+
+//     //constructor
+//     College(String name){
+//         this.name = name;
+//         students = new ArrayList<>();
+//     }
+
+//     public void addStudent(Student student){
+//         students.add(student);
+//     }
+//     public void printAllStudents(){
+//         for( int i = 0; i < students.size(); i++){
+//             Student student = students.get(i);
+//             System.out.println("id: " + student.getId() + ", Name: " + student.getName());
+//         }
+//     }
+// }
+
+//many to many relationship
+
+class Student{
+    private String name;
+    private List<Course> courses;
+
+    public Student(String name){
         this.name = name;
-        this.id = id;
+        courses = new ArrayList<>();
+    }
+
+    public void addCourse(Course course){
+        courses.add(course);
     }
 
     public String getName(){
         return name;
     }
 
-    public String getId(){
-        return id;
+    public void displayAllCourses(){
+        for(int i = 0; i < courses.size(); i++){
+            System.out.println(courses.get(i).getName());
+        }
     }
 }
 
-class College{
-    private String name;   
+class Course{
+    private String name;
     private List<Student> students;
 
-    //constructor
-    College(String name){
+    public Course(String name){
         this.name = name;
         students = new ArrayList<>();
     }
@@ -68,10 +114,14 @@ class College{
     public void addStudent(Student student){
         students.add(student);
     }
-    public void printAllStudents(){
-        for( int i = 0; i < students.size(); i++){
-            Student student = students.get(i);
-            System.out.println("id: " + student.getId() + ", Name: " + student.getName());
+
+    public String getName(){
+        return name;
+    }
+
+    public void displayAllStudents(){
+        for(int i = 0; i < students.size(); i++){
+            System.out.println(students.get(i).getName());
         }
     }
 }
@@ -88,15 +138,52 @@ class Association{
         // student.displayDetails();
 
         System.out.println();
-        System.out.println("------------ One-to-One Relationship ------------");
+        System.out.println("------------ One-to-Many Relationship ------------");
 
-        Student student1 = new Student("Alice ", "1");
-        Student student2 = new Student("Bob ", "2");
+        // Student student1 = new Student("Alice ", "1");
+        // Student student2 = new Student("Bob ", "2");
 
-        College college = new College("ABC");
-        college.addStudent(student1);
-        college.addStudent(student2);
-        college.printAllStudents();
+        // College college = new College("ABC");
+        // college.addStudent(student1);
+        // college.addStudent(student2);
+        // college.printAllStudents();
+
+
+        System.out.println();
+        System.out.println("------------ Many-to-Many Relationship ------------");
+
+        Course c1 = new Course("Maths");
+        Course c2 = new Course("CS");
+
+        Student s1 = new Student("Alice");
+        Student s2 = new Student("Bob");
+        Student s3 = new Student("Carl");
+
+        s1.addCourse(c1);
+        s1.addCourse(c2);
+        c1.addStudent(s1);
+        c1.addStudent(s2);
+
+        s2.addCourse(c1);
+        c1.addStudent(s2);
+
+        s3.addCourse(c2);
+        c2.addStudent(s3);
+
+        System.out.println("Students enrolled in Course 1:");
+        c1.displayAllStudents();
+        System.out.println();
+        System.out.println("Students enrolled in Course 2:");
+        c2.displayAllStudents();
+        System.out.println();
+        System.out.println("Student 1 is enrolled in following Courses:");
+        s1.displayAllCourses();
+        System.out.println();
+        System.out.println("Student 2 is enrolled in following Courses:");
+        s2.displayAllCourses();
+        System.out.println();
+        System.out.println("Student 3 is enrolled in following Courses:");
+        s3.displayAllCourses();
 
     }
 }
