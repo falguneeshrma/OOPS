288e3485de715bb2431d3a0e223fbd33dd3a0e60
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-05-19 23:56:22 +0530

    Custom Exceptions, Checked and Unchecked Exceptions

--- Advance_Programming/ExceptionHandling.java
+++ Advance_Programming/ExceptionHandling.java
@@ -1,26 +1,93 @@
 package Advance_Programming;
 import java.util.*;
 
-class ExceptionHandling{
-    private static void readFile() throws Exception{
-        throw new Exception();  // throw: it throws an error and does not use try and catch block here
+// class SelfMadeException extends Exception{
+//     SelfMadeException(String exception){
+//         super(exception);
+//         System.out.println("This is custom exception.");
+//     }
+// }
+
+
+//  Real World Example
+
+class InsufficientBalance extends Exception{
+    InsufficientBalance(String exception){
+        super(exception);
+        System.out.println("You do not have enough balance.");
     }
-    public static void main(String[] args) throws Exception{    // throws:it indicates there might be an expection
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(10);
-        arr.add(15);
+}
+
+
+class BankBalance{
+    String name;
+    double balance;
+
+    BankBalance(String name, double balance){
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void addBalance(double val){
+        balance += val;
+        System.out.println("Amount is credited.");
+    }
+
+    public void withdrawBalance(double val) throws InsufficientBalance{
+        if(val < balance){
+            balance -= val;
+            System.out.println("Amount is debited.");
+        }
+        else throw new InsufficientBalance("Your current available balance is: " + balance);
+    }
+}
+
+
+
+class ExceptionHandling{
+
+    // private static void readFile() throws Exception{
+    //     // throw new Exception();  // throw: it throws an error and does not use try and catch block here
+    //     throw new SelfMadeException("No lines of code");
+    // }
+
+    // public static void main(String[] args) throws Exception{    // throws: it indicates there might be an expection
+    //     ArrayList<Integer> arr = new ArrayList<>();
+    //     arr.add(10);
+    //     arr.add(15);
+    //     try{
+    //         System.out.println(arr.get(5)); //error statement
+    //         System.out.println("Statement1: after error statement");
+    //     }
+    //     catch(Exception e){
+    //         System.out.println("Catch statement 1" + e.getMessage());
+    //     }
+    //     finally{
+    //         System.out.println("Statement2: final statement");  // closing resources
+    //     }
+
+    //     try{
+    //         readFile(); // exception statement
+    //         System.out.println("Statement3: after exception statement");
+    //     }
+    //     catch(Exception e){
+    //         System.out.println("Catch statement 2 " + e.getMessage());
+    //     }
+        
+    // }
+
+
+    // Real World Example
+
+    public static void main(String[] args){
+        BankBalance cust1 = new BankBalance("Alice", 1000);
+        cust1.addBalance(100);
         try{
-            System.out.println(arr.get(5)); //error statement
-            System.out.println("Statement1: after error statement");
+            cust1.withdrawBalance(1000);
+            cust1.withdrawBalance(100);
         }
         catch(Exception e){
-            System.out.println("Catch statement " + e.getMessage());
+            System.out.println(e.getMessage());
         }
-        finally{
-            System.out.println("Statement2: final statement");  // closing resources
-        }
-
-        readFile();
-        System.out.println("Statement3: after exception statement");
     }
 }
