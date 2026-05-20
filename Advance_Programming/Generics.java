7f553826e12002e016dd27776dd50de671bf6b47
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-05-20 23:53:29 +0530

    Generics

---
+++ Advance_Programming/Generics.java
@@ -0,0 +1,11 @@
+package Advance_Programming;
+import java.util.*;
+
+class Generics{
+    public static void main(String[] args){
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Alice");
+    }
+}
