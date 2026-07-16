37521e2bedf716e4a5d11dc8414404f5c2cf0127
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-07-16 11:01:08 +0530

    Generic classes and methods

--- Advance_Programming/Generics.java
+++ Advance_Programming/Generics.java
@@ -1,11 +1,57 @@
 package Advance_Programming;
 import java.util.*;
 
-class Generics{
-    public static void main(String[] args){
-        ArrayList<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        ArrayList<String> list2 = new ArrayList<>();
-        list2.add("Alice");
+//Generic Classes
+
+class ExampleGenerics<T>{
+    private List<T> list = new ArrayList<>();
+
+    public void add(T val){
+        list.add(val);  //adds element
     }
+
+    public void removeLast(){
+        if(list.isEmpty()){
+            System.out.println("List is empty");
+        }
+        list.remove(list.size() - 1); //last index removed
+    }
+
+    public T getElement(int index){
+        return list.get(index); //gets element
+    }
+
+    public void print(){
+        for( T el : list){
+            System.out.print(el + " ");
+        }
+    }
+}
+
+//Generic Methods
+
+class GenericMethods {
+    public <T> void printArray(T[] arr){
+        for (T val : arr){
+            System.out.print(val + " ");
+        }
+    }
+}
+
+class Generics{
+
+    public static void main(String[] args){
+        ExampleGenerics<Integer> obj = new ExampleGenerics<Integer>();
+        obj.add(10);
+        obj.add(20);
+        System.out.println(obj.getElement(1));
+        obj.removeLast();
+        obj.print();
+        
+        System.out.println();
+
+        Integer[] arr = {1, 2, 3};
+        GenericMethods obj1 = new GenericMethods();
+        obj1.printArray(arr);
+    }   
 }
