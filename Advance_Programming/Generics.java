f9e0df1c412b9335dd7595313401881af1e05d08
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-08-02 10:18:40 +0530

    Bounded Type Parameters, Upper Bounded Type, Lower Bounded Type, Raw Type, Type Erasure

--- Advance_Programming/Generics.java
+++ Advance_Programming/Generics.java
@@ -46,18 +46,96 @@ class Calculator <T extends Number>{
     }
 } 
 
+class TypeErasure<T>{
+    T value;    
+
+    public void setVal(T value){
+        this.value = value;
+    }
+
+    public T getVal(){
+        return value;
+    }
+}
+//Java coverts the type 'T' as 'Object' during runtime
+class TypeErasure<Object>{
+    Object value;    
+
+    public void setVal(Object value){
+        this.value = value;
+    }
+
+    public Object getVal(){
+        return value;
+    }
+}
+
+// or
+
+
+//upper bound
+class TypeErasure<T extends Number>{
+    T value;    
+
+    public void setVal(T value){
+        this.value = value;
+    }
+
+    public T getVal(){
+        return value;
+    }
+}
+//Java coverts the type 'T' as 'Number' during runtime
+class TypeErasure<Number>{
+    Number value;    
+
+    public void setVal(Number value){
+        this.value = value;
+    }
+
+    public Number getVal(){
+        return value;
+    }
+}
+
+
+
+
+
+
 class Generics{
 
+    //upper bound
     public static void printArray(List<? extends Number> list){  //only read access
         for( Number val : list){
             System.out.print(val + " ");
         }
     }
 
+
+    //lower bound
+    public static void writeArray(List<? super Number> list){
+        list.add(10);
+        list.get(0);                        //read access given //read and store is not allowed
+        System.out.println(list.get(0));
+
+    }
+    
+
     public static void main(String[] args){
         List<Number> list = new ArrayList<>();
         list.add(10);
         list.add(100);
         printArray(list);
+        writeArray(list);
+
+        //raw type 
+        List ls = new ArrayList<>();
+        ls.add(10);
+        ls.add(10.9);
+        ls.add("hello");
+        for(Object obj : ls){
+            System.out.println(obj);
+        }
     }   
 }
