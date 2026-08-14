4a2102e2b15aa837889b9ebe942cde0581cb9197
Falguni Sharma <falguneeshrma2005@gmail.com>
2026-08-14 23:33:53 +0530

    Interface Segregation Principle

--- DesignAndLifecycleManagement/SOLID_DesignPrinciples.java
+++ DesignAndLifecycleManagement/SOLID_DesignPrinciples.java
@@ -53,6 +53,20 @@ class Penguin {
     // Penguins do not implement FlyingBird
 }
 
+// ISP
+
+interface Worker {
+    void work();
+    void eat();
+}
+
+class Robot implements Worker {
+    public void work() { System.out.println("Robot working"); }
+    public void eat() { throw new UnsupportedOperationException("Robots don't eat!"); }
+}
+
+
+
 
 
 
